e56f0ba309577df02ef564727f51c8f65671260c
michoff <sergofiles-16@mail.ru>
2023-09-06 19:05:09 +0300

    ---

--- game/Play_game.py
+++ game/Play_game.py
@@ -1,7 +1,18 @@
+import pygame
+import sys
+
+
 def game():
-    running = True
-    draw_text('GAME SCREEN', font, (255, 255, 255), screen, 20, 20)
-    while running:
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("Dark Moon")
+    clock = pygame.time.Clock()
+    click = False
+    level_pick = pygame.image.load('images/stoneheart_1.jpg')
+    screen.blit(level_pick, (0, 0))
+
+    while True:
         mx, my = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -9,7 +20,7 @@ def game():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    running = False
+                    break
 
         pygame.display.update()
         clock.tick(60)
